3ac3c1e62b009b044b8174f10625820e9f57bf37
Jonathan Poch <jonathan.poch@nyu.edu>
2017-07-18 10:06:26 -0400

    Added project files

---
+++ BaseSystemConverter/src/BaseSystemConverter/ConverterUtil.java
@@ -0,0 +1,12 @@
+package BaseSystemConverter;
+public class ConverterUtil {
+
+	/**
+	 * @param args
+	 */
+	
+	public static String convert ( int x, int radix )
+	{
+		return Integer.toString(x,radix);
+  }
+}
